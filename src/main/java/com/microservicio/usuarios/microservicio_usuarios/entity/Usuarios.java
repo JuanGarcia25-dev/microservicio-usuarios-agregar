fba00715afd42243428f387dbb5f7b1c460607d6
Adonai-24 <2022371095@uteq.edu.mx>
2025-11-07 18:57:17 -0600

    Entity de usuarios para la base de datos

--- src/main/java/com/microservicio/usuarios/microservicio_usuarios/entity/Usuarios.java
+++ src/main/java/com/microservicio/usuarios/microservicio_usuarios/entity/Usuarios.java
@@ -1,5 +1,40 @@
+/*****************************************************************************
+ * @Author                : adolnb<loro.lorenzonunez@gmail.com>              *
+ * @CreatedDate           : 2025-11-07 18:37:21                              *
+ * @LastEditors           : adolnb<loro.lorenzonunez@gmail.com>              *
+ * @LastEditDate          : 2025-11-07 18:37:21                              *
+ * @FilePath              : Usuarios.java                                    *
+ * @CopyRight             : © 2025 Adonai LN - B0MB0                         *
+ ****************************************************************************/
+
 package com.microservicio.usuarios.microservicio_usuarios.entity;
 
+import java.sql.Array;
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "t_usuarios")
 public class Usuarios {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "matricula", nullable = true, unique = true)
+    private String matricula;
+    
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+    
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+    
+    @Column(name = "rol", nullable = false)
+    private Array rol;
+    
+    @Column(name = "estatus", nullable = false)
+    private Boolean estatus;
 }
